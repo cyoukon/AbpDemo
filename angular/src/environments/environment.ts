521fb25573331c2fe97affb7d4e225e89f560d70
zhao kun <1162289133@qq.com>
2022-01-23 21:41:36 +0800

    init

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'AbpDemo',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44304',
+    redirectUri: baseUrl,
+    clientId: 'AbpDemo_App',
+    responseType: 'code',
+    scope: 'offline_access AbpDemo',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44384',
+      rootNamespace: 'AbpDemo',
+    },
+  },
+} as Environment;
